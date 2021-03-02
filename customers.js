743fcd18f248fc43850b0860d8d68d4415558e7e
hayden-cole <haydencole@protonmail.com>
2021-03-01 18:32:55 -0800

    Customers page loads, nonfunctional as of now. No table formatting.

--- customers.js
+++ customers.js
@@ -3,7 +3,7 @@ module.exports = function () {
     var router = express.Router();
 
     function getCustomers(res, mysql, context, complete) {
-        mysql.pool.query("SELECT customerID, firstName, lastName, phone, addressLine1, addressLine2, city, state, zip, currentBill FROM customers", function (error, results, fields) {
+        mysql.pool.query("SELECT customerID, firstName, lastName, phone, address1, address2, city, state, zip, currentBill FROM customers", function (error, results, fields) {
             if (error) {
                 res.write(JSON.stringify(error));
                 res.end();
@@ -35,9 +35,9 @@ module.exports = function () {
     router.post('/', function (req, res) {
         console.log("Adding a customer")
         var mysql = req.app.get('mysql');
-        var sql = "INSERT INTO customers (firstName, lastName, phone, addressLine1, addressLine2, city, state, zip, currentBill) VALUES (?,?,?,?,?,?,?,?,?)";
-        var inserts = [req.body.firstName, req.body.lastName, req.body.phone, req.body.addressLine1
-            , req.body.addressLine2, req.body.city, req.body.state, req.body.zip, req.body.currentBill];
+        var sql = "INSERT INTO customers (firstName, lastName, phone, address1, address2, city, state, zip, currentBill) VALUES (?,?,?,?,?,?,?,?,?)";
+        var inserts = [req.body.firstName, req.body.lastName, req.body.phone, req.body.address1
+            , req.body.address2, req.body.city, req.body.state, req.body.zip, req.body.currentBill];
         sql = mysql.pool.query(sql, inserts, function (error, results, fields) {
             if (error) {
                 console.log(JSON.stringify(error))
