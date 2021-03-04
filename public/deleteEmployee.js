0902678f5ed7913ccbc1a4ea53c6d2f0301a80d6
hayden-cole <haydencole@protonmail.com>
2021-03-03 19:14:41 -0800

    Removed duplicate files

--- public/deleteEmployee.js
+++
@@ -1,9 +0,0 @@
-function deleteEmployee(id){
-    $.ajax({
-        url: '/employees/' + id,
-        type: 'DELETE',
-        success: function(result){
-            window.location.reload(true);
-        }
-    })
-};
--- public/deletePayment.js
+++
@@ -1,9 +0,0 @@
-function deletePayment(id) {
-    $.ajax({
-        url: '/payments/' + id,
-        type: 'DELETE',
-        success: function (result) {
-            window.location.reload(true);
-        }
-    })
-};
